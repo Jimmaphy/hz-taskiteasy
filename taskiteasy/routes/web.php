e97a7f6fc206bb2d10432a229c8ea99068100365
Wesley van Schaijk <wschaijk@outlook.com>
2023-12-27 15:55:32 +0100

    Task 1.1C - Showing Tasks

--- taskiteasy/routes/web.php
+++ taskiteasy/routes/web.php
@@ -20,3 +20,62 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/tasks/{id}', function ($id) {
+    $task = findTask($id);
+
+    if (!$task) {
+        abort(404);
+    }
+
+    return view('tasks.show', [
+        'task' => findTask($id)
+    ]);
+});
+
+function findTask($id): array|null {
+    $data = [
+        [
+            "id" => 1,
+            "title" => "Write a love letter to Laravel",
+            "description" => "Compose a heartfelt love letter to Laravel, explaining how it has revolutionized your coding",
+            "priority" => 3,
+            "state" => "new",
+            "time_estimated" => 15,
+            "time_spent" => 0,
+            "created_at" => "2023-10-15 09:00:00",
+            "updated_at" => "2023-10-15 09:30:00",
+            "completed_at" => null
+        ],
+        [
+            "id" => 2,
+            "title" => "Take out the trash",
+            "description" => "It's starting to stink in the kitchen, throw out that trash",
+            "priority" => 1,
+            "state" => "new",
+            "time_estimated" => 2,
+            "time_spent" => 0,
+            "created_at" => "2023-10-15 09:00:00",
+            "updated_at" => "2023-10-15 09:30:00",
+            "completed_at" => null
+        ],
+        [
+            "id" => 3,
+            "title" => "Finish the Laravel Assignment",
+            "description" => "You need to spend time on this exercises, for the students sake",
+            "priority" => 3,
+            "state" => "new",
+            "time_estimated" => 150,
+            "time_spent" => 0,
+            "created_at" => "2023-10-15 09:00:00",
+            "updated_at" => "2023-10-15 09:30:00",
+            "completed_at" => null
+        ]
+    ];
+
+    $filtered = array_filter($data, function ($child) use ($id) {
+        return $child['id'] == $id;
+    });
+
+    return count($filtered) > 0 ? reset($filtered) : null;
+}
