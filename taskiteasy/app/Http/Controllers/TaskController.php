f2e8f37c249172f650a0c665b66c64c6e06cb67f
Wesley van Schaijk <wschaijk@outlook.com>
2023-12-29 13:52:32 +0100

    Task 2.1D - The Task Migration

--- taskiteasy/app/Http/Controllers/TaskController.php
+++ taskiteasy/app/Http/Controllers/TaskController.php
@@ -2,60 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\Task;
 use Illuminate\View\View;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class TaskController extends Controller
 {
-    private array $data = [
-        [
-            "id" => 1,
-            "title" => "Write a love letter to Laravel",
-            "description" => "Compose a heartfelt love letter to Laravel, explaining how it has revolutionized your coding",
-            "priority" => 1,
-            "state" => "new",
-            "time_estimated" => 15,
-            "time_spent" => 0,
-            "created_at" => "2023-10-15 09:00:00",
-            "updated_at" => "2023-10-15 09:30:00",
-            "completed_at" => null
-        ],
-        [
-            "id" => 2,
-            "title" => "Take out the trash",
-            "description" => "It's starting to stink in the kitchen, throw out that trash",
-            "priority" => 1,
-            "state" => "complete",
-            "time_estimated" => 3,
-            "time_spent" => 2,
-            "created_at" => "2023-10-15 09:00:00",
-            "updated_at" => "2023-10-15 09:30:00",
-            "completed_at" => "2023-10-15 10:00:00"
-        ],
-        [
-            "id" => 3,
-            "title" => "Finish the Laravel Assignment",
-            "description" => "You need to spend time on this exercises, for the students sake",
-            "priority" => 4,
-            "state" => "started",
-            "time_estimated" => 150,
-            "time_spent" => 160,
-            "created_at" => "2023-10-15 09:00:00",
-            "updated_at" => "2023-10-15 09:30:00",
-            "completed_at" => null
-        ]
-    ];
-
     /**
      * Returns the view for the index page.
      * This page contains all the tasks.
      *
      * @return View The view for the index page.
      */
-    public function index(): View {
+    public function index(): View
+    {
         return view('tasks.index', [
-            'tasks' => DB::table('tasks')->get()
+            'tasks' => Task::get()
         ]);
     }
 
@@ -66,26 +28,11 @@ class TaskController extends Controller
      * @return View The view for displaying the task.
      * @throws NotFoundHttpException When the requested ID does not exist.
      */
-    public function show(string $id): View {
-        $task = DB::table('tasks')->where('id', '=', $id)->get();
-
-        if(count($task) !== 1) {
-            abort(404);
-        }
-
+    public function show(string $id): View
+    {
+        $task = Task::findOrFail($id);
         return view('tasks.show', [
-            'task' => $task[0]
+            'task' => $task
         ]);
-    }
-
-    /**
-     * Find a task by its ID.
-     *
-     * @param mixed $id The ID of the task to find.
-     * @return array|null The found task as an associative array if found, otherwise null.
-     */
-    private function findTask($id): array|null {
-        $key = array_search($id, array_column($this->data, 'id'));
-        return $key !== false ? $this->data[$key] : null;
     }
 }
---
+++ taskiteasy/app/Models/Task.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Task
+ *
+ * @property int $id
+ * @property string|null $title
+ * @property string|null $description
+ * @property int|null $priority
+ * @property string|null $state
+ * @property int|null $time_estimated
+ * @property int|null $time_spent
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $completed_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Task newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Task newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Task query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereCompletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task wherePriority($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereState($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereTimeEstimated($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereTimeSpent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Task extends Model
+{
+    use HasFactory;
+}
